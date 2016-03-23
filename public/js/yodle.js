c2e2a9caaaf776443bbf26eaeef8bafeb47442f4
Dominic Prescod <dominicprescod@gmail.com>
2016-03-23 10:32:08 -0400

    changes

--- public/js/yodle.js
+++ public/js/yodle.js
@@ -63,20 +63,20 @@ $.ajax({
       var score = (jugglers[f].H * circuits[parseInt(jugglers[f].cPref[e])].H)+(jugglers[f].E * circuits[parseInt(jugglers[f].cPref[e])].E)+(jugglers[f].P * circuits[parseInt(jugglers[f].cPref[e])].P);
       // console.log(score);
       jugglers[f].scores.push(score);
-      topScores[parseInt(jugglers[f].cPref[e])].push({
-            score: score,
-            jugg: f
-          });
+      // topScores[parseInt(jugglers[f].cPref[e])].push({
+      //       score: score,
+      //       jugg: f
+      //     });
     }
     for(var t = 0; t < circuits.length; t++){
         if(!isInArray(t.toString(), jugglers[f].cPref)){
           var scor = (jugglers[f].H * circuits[t].H)+(jugglers[f].E * circuits[t].E)+(jugglers[f].P * circuits[t].P);
           jugglers[f].scores.push(scor);
 //           // jugglers[f].cPref.push(t.toString());
-          topScores[t].push({
-                score: scor,
-                jugg: f
-              });
+          // topScores[t].push({
+          //       score: scor,
+          //       jugg: f
+          //     });
         }
       }
   }
@@ -98,144 +98,144 @@ $.ajax({
 //
 //
 // // sorting the scores in descending order
-  topScores.forEach(function(z){
-    z.sort(function(a, b) {
-      return b.score - a.score;
-  });
-});
-// // // setting a variable to check if all jugglers have been assigned
-var assigned  = 0;
+//   topScores.forEach(function(z){
+//     z.sort(function(a, b) {
+//       return b.score - a.score;
+//   });
+// });
+// // // // setting a variable to check if all jugglers have been assigned
+// var assigned  = 0;
+// //
+// // // checking against the total amount of jugglers
+// while(assigned < jugglers.length){
+// // loops through each circuit by topScores
+//   for(var i = 0; i < topScores.length; i++){
+// //     // if circuit is not filled/fully assigned
+//     if(circuits[i].assigned.length < jugglers.length / circuits.length){
+//       if(jugglers[topScores[i][0].jugg].cPref > 0){
+//       // if the top score for the circuit is also the top preference for the juggler, place the juggler into the circuit
+//         if(parseInt(jugglers[topScores[i][0].jugg].cPref[0]) === i){
+//           // stores the juggler id to remove their scores from ALL circuits
+//           var who = topScores[i][0].jugg;
 //
-// // checking against the total amount of jugglers
-while(assigned < jugglers.length){
-// loops through each circuit by topScores
-  for(var i = 0; i < topScores.length; i++){
-//     // if circuit is not filled/fully assigned
-    if(circuits[i].assigned.length < jugglers.length / circuits.length){
-      if(jugglers[topScores[i][0].jugg].cPref > 0){
-      // if the top score for the circuit is also the top preference for the juggler, place the juggler into the circuit
-        if(parseInt(jugglers[topScores[i][0].jugg].cPref[0]) === i){
-          // stores the juggler id to remove their scores from ALL circuits
-          var who = topScores[i][0].jugg;
-
-          // removing all the placed jugglers scores from the topScores
-          // =================================================================
-            topScores.forEach(function(b){
-              for(var z = 0; z < b.length; z++){
-                if(who === b[z].jugg){
-                  b.splice(z,1);
-                }
-              }
-            });
-          // =================================================================
-          // assigning the juggler to the circuit
-          circuits[i].assigned.push(jugglers[who]);
-          // noting that the juggler was assigned
-          assigned++;
-
-          // if the circuit is fully assigned
-          // remove the circuit preference from ALL jugglers so their second/third choice becomes their first
-          if(circuits[i].assigned.length === jugglers.length/circuits.length){
-            jugglers.forEach(function(b){
-              for(var r = 0; r < b.cPref.length; r++){
-                if(parseInt(b.cPref[r])=== i ){
-                  b.cPref.splice(r,1);
-                }
-              }
-            });
-          }
-        }
-      } else {//====
-          // stores the juggler id to remove their scores from ALL circuits
-          var who = topScores[i][0].jugg;
-
-          // removing all the placed jugglers scores from the topScores
-          // =================================================================
-            topScores.forEach(function(b){
-              for(var z = 0; z < b.length; z++){
-                if(who === b[z].jugg){
-                  b.splice(z,1);
-                }
-              }
-            });
-          // =================================================================
-          // assigning the juggler to the circuit
-          circuits[i].assigned.push(jugglers[who]);
-          // noting that the juggler was assigned
-          assigned++;
-
-          // if the circuit is fully assigned
-          // remove the circuit preference from ALL jugglers so their second/third choice becomes their first
-          if(circuits[i].assigned.length === jugglers.length/circuits.length){
-            jugglers.forEach(function(b){
-              for(var r = 0; r < b.cPref.length; r++){
-                if(parseInt(b.cPref[r])=== i ){
-                  b.cPref.splice(r,1);
-                }
-              }
-            });
-          }
-
-      }//======
-    }
-  }
-}
-// setting variables to store the final list to be downloaded
-var assignments = [];
-var list = [];
-var c1970 = 0;
-// sets the inner empty arrays to the length of the total amount of circuits
-for(var p = 0; p < circuits.length; p++){
-  assignments.push([]);
-  list.push('\nC'+p+' ->');
-}
-
-// Creates a concated string of the ranked jugglers per circuit
-for(var h = 0; h < circuits.length; h++){
-  // 0,1,2 Circuits
-  var sum  = 0;
-  for(var j = 0; j < circuits[h].assigned.length; j++){
-    // assigned
-    assignments[h].push(' J'+circuits[h].assigned[j].id);
-    sum += circuits[h].assigned[j].id;
-    for(var l = 0; l < circuits[h].assigned[j].pref.length; l++){
-      // pref
-      assignments[h][j] = assignments[h][j].concat(' C'+circuits[h].assigned[j].pref[l]+':'+circuits[h].assigned[j].scores[l]);
-    }
-  }
-  if(h === 1970) c1970 = sum;
-}
-// creates the final list with Circuit assignment at the start of the string
-// Jugglers for that Circuit
-// The sum of all jugglers for that circuit as the last element of the string
-for(var s = 0; s < list.length; s++){
-  list[s] = list[s].concat(assignments[s]);
-}
-if(c1970 > 0) list.unshift(c1970);
-// Download the file
-// On click of the HTML page downloads a link of the Circuit assignments
-// ===========================================================================================================
-function downloadSorted(filename, mimeType) {
-    var elHtml = list;
-    var link = document.createElement('a');
-    mimeType = mimeType || 'text/plain';
-
-    link.setAttribute('download', filename);
-    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
-    link.click();
-}
-
-var fileName =  'jugglers-circuit-assigment.txt';
-
-$('h1').click(function(){
-    downloadSorted(fileName, 'main','text/html');
-});
-// ===========================================================================================================
+//           // removing all the placed jugglers scores from the topScores
+//           // =================================================================
+//             topScores.forEach(function(b){
+//               for(var z = 0; z < b.length; z++){
+//                 if(who === b[z].jugg){
+//                   b.splice(z,1);
+//                 }
+//               }
+//             });
+//           // =================================================================
+//           // assigning the juggler to the circuit
+//           circuits[i].assigned.push(jugglers[who]);
+//           // noting that the juggler was assigned
+//           assigned++;
+//
+//           // if the circuit is fully assigned
+//           // remove the circuit preference from ALL jugglers so their second/third choice becomes their first
+//           if(circuits[i].assigned.length === jugglers.length/circuits.length){
+//             jugglers.forEach(function(b){
+//               for(var r = 0; r < b.cPref.length; r++){
+//                 if(parseInt(b.cPref[r])=== i ){
+//                   b.cPref.splice(r,1);
+//                 }
+//               }
+//             });
+//           }
+//         }
+//       } else {//====
+//           // stores the juggler id to remove their scores from ALL circuits
+//           var who = topScores[i][0].jugg;
+//
+//           // removing all the placed jugglers scores from the topScores
+//           // =================================================================
+//             topScores.forEach(function(b){
+//               for(var z = 0; z < b.length; z++){
+//                 if(who === b[z].jugg){
+//                   b.splice(z,1);
+//                 }
+//               }
+//             });
+//           // =================================================================
+//           // assigning the juggler to the circuit
+//           circuits[i].assigned.push(jugglers[who]);
+//           // noting that the juggler was assigned
+//           assigned++;
+//
+//           // if the circuit is fully assigned
+//           // remove the circuit preference from ALL jugglers so their second/third choice becomes their first
+//           if(circuits[i].assigned.length === jugglers.length/circuits.length){
+//             jugglers.forEach(function(b){
+//               for(var r = 0; r < b.cPref.length; r++){
+//                 if(parseInt(b.cPref[r])=== i ){
+//                   b.cPref.splice(r,1);
+//                 }
+//               }
+//             });
+//           }
+//
+//       }//======
+//     }
+//   }
+// }
+// // setting variables to store the final list to be downloaded
+// var assignments = [];
+// var list = [];
+// var c1970 = 0;
+// // sets the inner empty arrays to the length of the total amount of circuits
+// for(var p = 0; p < circuits.length; p++){
+//   assignments.push([]);
+//   list.push('\nC'+p+' ->');
+// }
+//
+// // Creates a concated string of the ranked jugglers per circuit
+// for(var h = 0; h < circuits.length; h++){
+//   // 0,1,2 Circuits
+//   var sum  = 0;
+//   for(var j = 0; j < circuits[h].assigned.length; j++){
+//     // assigned
+//     assignments[h].push(' J'+circuits[h].assigned[j].id);
+//     sum += circuits[h].assigned[j].id;
+//     for(var l = 0; l < circuits[h].assigned[j].pref.length; l++){
+//       // pref
+//       assignments[h][j] = assignments[h][j].concat(' C'+circuits[h].assigned[j].pref[l]+':'+circuits[h].assigned[j].scores[l]);
+//     }
+//   }
+//   if(h === 1970) c1970 = sum;
+// }
+// // creates the final list with Circuit assignment at the start of the string
+// // Jugglers for that Circuit
+// // The sum of all jugglers for that circuit as the last element of the string
+// for(var s = 0; s < list.length; s++){
+//   list[s] = list[s].concat(assignments[s]);
+// }
+// if(c1970 > 0) list.unshift(c1970);
+// // Download the file
+// // On click of the HTML page downloads a link of the Circuit assignments
+// // ===========================================================================================================
+// function downloadSorted(filename, mimeType) {
+//     var elHtml = list;
+//     var link = document.createElement('a');
+//     mimeType = mimeType || 'text/plain';
+//
+//     link.setAttribute('download', filename);
+//     link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+//     link.click();
+// }
+//
+// var fileName =  'jugglers-circuit-assigment.txt';
+//
+// $('h1').click(function(){
+//     downloadSorted(fileName, 'main','text/html');
+// });
+// // ===========================================================================================================
 //
 //
 //
-console.log(assignments);
-// // console.log(jugglers);
+// console.log(assignments);
+console.log(jugglers);
 // // console.log(response);
 // // console.log(list[0].concat(assignments[0]));
 // // console.log(list[1].concat(assignments[1]));
